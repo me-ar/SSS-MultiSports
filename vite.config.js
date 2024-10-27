2f7a6f2f2b7d1995c180e0b78b532c85211fb7ce
me-ar <91774024+me-ar@users.noreply.github.com>
2024-10-27 22:25:41 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
 
     build: {
-        outDir: "build",
+        outDir: 'dist',
     },
     plugins: [tsconfigPaths(), react()],
 });
